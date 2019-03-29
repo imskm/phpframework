f9cc91d72ae908681315f12cb75d318d186522ff
Shek Muktar <im.skm07@gmail.com>
2019-03-29 18:36:49 +0530

    Added new test cases to test Jolo API account balance check and Beneficiary bank account correctness check

--- Tests/JoloApi/JoloBankTest.php
+++ Tests/JoloApi/JoloBankTest.php
@@ -75,7 +75,7 @@ class JoloBankTest extends BaseTest
 			'phone' 				=> '',
 			'name'					=> '',
 			'beneficiary_ifsc' 		=> '',
-			'beneficiary_acct_no' 	=> '',
+			'beneficiary_acct_no'	=> '',
 		])->create();
 
 		// OR 4.1 Create beneficiary
@@ -130,7 +130,7 @@ class JoloBankTest extends BaseTest
 		$result = $this->jolo->from([
 			'phone' 				=> '',
 			'beneficiary_id' 		=> '',
-			'order_id' 				=> '',
+			'orderid' 				=> '',
 			'amount' 				=> '',
 			'remarks' 				=> '',
 		])->transfer();
@@ -139,10 +139,29 @@ class JoloBankTest extends BaseTest
 		$result = $this->jolo->create(new JoloBankTransfer([
 			'phone' 				=> '',
 			'beneficiary_id' 		=> '',
-			'order_id' 				=> '',
+			'orderid' 				=> '',
 			'amount' 				=> '',
 			'remarks' 				=> '',
 		]));
+
+		// 9
+		// 
+
+		// 9.1 Check Jolo Api Balance
+		//     no parameters are required for balance check, but api key is
+		//     required and it will be sent be JoloBank class
+		$result = $this->jolo->check('balance');
+
+		// 10
+		// 
+
+		// 10.1 Check bank account by transferring a small amount
+		$result = $this->jolo->check('bank', [
+			'phone'						=> '',
+			'beneficiary_account_no'	=> '',
+			'beneficiary_ifsc'			=> '',
+			'orderid'					=> '',
+		]);
 
 
 
@@ -163,10 +182,10 @@ class JoloBankTest extends BaseTest
 	public function test_jolo_api_create_new_agent()
 	{
 		$result = $this->jolo_bank->create(new JoloAgent([
-			'phone' 	=> '6290342363',
-			'name'		=> 'shek muktar',
-			'email' 	=> 'gainacs@gmail.com',
-			'address' 	=> 'H.No-25/2, N.C. Road, Kankinara, P.O.-Kankinara, PIN-743126, West Bengal',
+			'phone' 	=> '9992223335',
+			'name'		=> 'Super Man',
+			'email' 	=> 'superman@gmail.com',
+			'address' 	=> 'H.No-32, G.K. Road, FakeTown, P.O.-FakeTown, PIN-123456, Some State',
 		]));
 
 		if (!$result) {
@@ -181,7 +200,7 @@ class JoloBankTest extends BaseTest
 	public function test_jolo_api_verify_new_agent()
 	{
 		$result = $this->jolo_bank->verify('agent', [
-			'phone' => '6290342363',
+			'phone' => '9992223335',
 			'otp'   => '1234'
 		]);
 
@@ -199,7 +218,7 @@ class JoloBankTest extends BaseTest
 	public function test_jolo_api_verify_new_agent_fails_due_to_wrong_otp()
 	{
 		$result = $this->jolo_bank->verify('agent', [
-			'phone' => '629034236',
+			'phone' => '6290342363',
 			'otp'   => '12345'
 		]);
 
@@ -217,8 +236,8 @@ class JoloBankTest extends BaseTest
 	public function test_jolo_api_create_new_beneficiary_for_registered_agent()
 	{
 		$result = $this->jolo_bank->create(new JoloBeneficiary([
-			'phone' 				=> '6290342363',		/* regd. agent phone number */
-			'beneficiary_name'		=> 'sunny chaudhary',
+			'phone' 				=> '9992223335',		/* regd. agent phone number */
+			'beneficiary_name'		=> 'bat man',
 			'beneficiary_ifsc' 		=> '5454554',
 			'beneficiary_account_no'=> '98464654654654',
 		]));
@@ -238,7 +257,7 @@ class JoloBankTest extends BaseTest
 	public function test_jolo_api_verify_created_beneficiary()
 	{
 		$result = $this->jolo_bank->verify('beneficiary', [
-			'phone' 				=> '6290342363',		/* regd. agent phone number */
+			'phone' 				=> '9992223335',		/* regd. agent phone number */
 			'beneficiaryid'		=> '98464654654654_5454554',
 			'otp'				=> '1234',
 		]);
@@ -262,7 +281,7 @@ class JoloBankTest extends BaseTest
 	public function test_jolo_api_verify_fails_for_non_existing_beneficiary()
 	{
 		$result = $this->jolo_bank->verify('beneficiary', [
-			'phone' 				=> '6290342363',		/* regd. agent phone number */
+			'phone' 				=> '9992223335',		/* regd. agent phone number */
 			'beneficiaryid'		=> '98464654654654_54545546', /* invalid */
 			'otp'				=> '1234',
 		]);
@@ -284,7 +303,7 @@ class JoloBankTest extends BaseTest
 	public function test_jolo_api_create_new_transfer_for_registered_beneficiary()
 	{
 		$result = $this->jolo_bank->create(new JoloBankTransfer([
-			'phone' 				=> '6290342363',		/* regd. agent phone number */
+			'phone' 				=> '9992223335',		/* regd. agent phone number */
 			'beneficiaryid'			=> '98464654654654_5454554',
 			'orderid' 				=> '123',
 			'amount'				=> 4000,
@@ -303,10 +322,10 @@ class JoloBankTest extends BaseTest
 		return true;
 	}
 
-	public function test_jolo_api_verify_transfer_was_successful()
+	public function test_jolo_api_verify_transfer_was_successful($txn)
 	{
 		$result = $this->jolo_bank->verify('transfer', [
-			'txn' 				=> 'A201903261899962522',	/* transaction # came as res */
+			'txn' 				=> $txn,	/* transaction # came as res */
 		]);
 
 		if (!$result) {
@@ -329,7 +348,7 @@ class JoloBankTest extends BaseTest
 	public function test_jolo_api_verify_transfer_status_with_wrong_txn()
 	{
 		$result = $this->jolo_bank->verify('transfer', [
-			'txn' 				=> 'B201903261899962522',	/* transaction # came as res */
+			'txn' 				=> 'B201903261899962522',
 		]);
 
 		if ($result) {
@@ -347,7 +366,7 @@ class JoloBankTest extends BaseTest
 	public function test_jolo_api_get_detail_of_registered_agent()
 	{
 		$result = $this->jolo_bank->detail('agent', [
-			'phone' 			=> '6290342363',
+			'phone' 			=> '9992223335',
 		]);
 
 		if (!$result) {
@@ -433,10 +452,47 @@ class JoloBankTest extends BaseTest
 
 		return true;
 	}
+
+	public function test_jolo_api_balance_check()
+	{
+		$result = $this->jolo_bank->check('balance');
+
+		if (!$result) {
+			$this->failed();
+			print_r($this->jolo_bank->getResponse());
+			return false;
+		}
+		$this->passed();
+
+		print_r($this->jolo_bank->getResponse());
+
+		return true;
+	}
+
+	public function test_jolo_api_bank_account_correctness_check()
+	{
+		$result = $this->jolo_bank->check('bank', [
+			'phone' 			=> '9992223335',	/* regd. agent phone number */
+			'beneficiary_account_no'=> '98464654654654_5454554',
+			'beneficiary_ifsc' 	=> '5454554',				
+			'orderid'			=> 123,				/* set by my website */
+		]);
+
+		if (!$result) {
+			$this->failed();
+			print_r($this->jolo_bank->getResponse());
+			return false;
+		}
+		$this->passed();
+
+		print_r($this->jolo_bank->getResponse());
+
+		return true;
+	}
 }
 
 
-$test = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 
 // TEST #1 : test_jolo_bank_class_intantiated_properly
 echo "TEST #1: test_jolo_bank_class_intantiated_properly: ";
@@ -446,70 +502,80 @@ $test->test_jolo_bank_class_intantiated_properly();
 echo "TEST #2: test_jolo_api_create_new_agent: ";
 $test->test_jolo_api_create_new_agent();
 
-$test3 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test3 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 // TEST #3 : test_jolo_api_verify_new_agent
 echo "TEST #3: test_jolo_api_verify_new_agent: ";
 $test3->test_jolo_api_verify_new_agent();
 
-$test4 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test4 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 // TEST #4 : test_jolo_api_verify_new_agent_fails_due_to_wrong_otp
 echo "TEST #4: test_jolo_api_verify_new_agent_fails_due_to_wrong_otp: ";
 $test4->test_jolo_api_verify_new_agent_fails_due_to_wrong_otp();
 
-$test5 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test5 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 // TEST #5 : test_jolo_api_create_new_beneficiary_for_registered_agent
 echo "TEST #5: test_jolo_api_create_new_beneficiary_for_registered_agent: ";
 $test5->test_jolo_api_create_new_beneficiary_for_registered_agent();
 
-$test6 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test6 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 // TEST #6 : test_jolo_api_verify_created_beneficiary
 echo "TEST #6: test_jolo_api_verify_created_beneficiary: ";
 $test6->test_jolo_api_verify_created_beneficiary();
 
-$test7 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test7 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 // TEST #7 : test_jolo_api_verify_fails_for_non_existing_beneficiary
 echo "TEST #7: test_jolo_api_verify_fails_for_non_existing_beneficiary: ";
 $test7->test_jolo_api_verify_fails_for_non_existing_beneficiary();
 
-$test8 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test8 = new JoloBankTest($jb = new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 // TEST #8 : test_jolo_api_create_new_transfer_for_registered_beneficiary
 echo "TEST #8: test_jolo_api_create_new_transfer_for_registered_beneficiary: ";
 $test8->test_jolo_api_create_new_transfer_for_registered_beneficiary();
 
-$test9 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test9 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 // TEST #9 : test_jolo_api_verify_transfer_was_successful
 echo "TEST #9: test_jolo_api_verify_transfer_was_successful: ";
-$test9->test_jolo_api_verify_transfer_was_successful();
+$test9->test_jolo_api_verify_transfer_was_successful($jb->getResponse()->txid);
 
-$test10 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test10 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 // TEST #10 : test_jolo_api_verify_transfer_status_with_wrong_txn
 echo "TEST #10: test_jolo_api_verify_transfer_status_with_wrong_txn: ";
 $test10->test_jolo_api_verify_transfer_status_with_wrong_txn();
 
-$test11 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test11 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 // TEST #11 : test_jolo_api_get_detail_of_registered_agent
 echo "TEST #11: test_jolo_api_get_detail_of_registered_agent: ";
 $test11->test_jolo_api_get_detail_of_registered_agent();
 
-$test12 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test12 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 // TEST #12 : test_jolo_api_get_detail_fails_for_unregistered_agent
 echo "TEST #12: test_jolo_api_get_detail_fails_for_unregistered_agent: ";
 $test12->test_jolo_api_get_detail_fails_for_unregistered_agent();
 
-$test13 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test13 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 // TEST #13 : test_jolo_api_get_detail_of_registered_beneficiary
 echo "TEST #13: test_jolo_api_get_detail_of_registered_beneficiary: ";
 $test13->test_jolo_api_get_detail_of_registered_beneficiary();
 
-$test14 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test14 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 // TEST #14 : test_jolo_api_get_detail_fails_for_unregistered_beneficiary
 echo "TEST #14: test_jolo_api_get_detail_fails_for_unregistered_beneficiary: ";
 $test14->test_jolo_api_get_detail_fails_for_unregistered_beneficiary();
 
-$test15 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "205256825787787", 0));
+$test15 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
 // TEST #15 : test_jolo_api_delete_registered_beneficiary
 echo "TEST #15: test_jolo_api_delete_registered_beneficiary: ";
 $test15->test_jolo_api_delete_registered_beneficiary();
+
+$test16 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
+// TEST #16 : test_jolo_api_balance_check
+echo "TEST #16: test_jolo_api_balance_check: ";
+$test16->test_jolo_api_balance_check();
+
+$test17 = new JoloBankTest(new JoloBank(new Phurl, $api_key = "322051306294169", 0));
+// TEST #17 : test_jolo_api_bank_account_correctness_check
+echo "TEST #17: test_jolo_api_bank_account_correctness_check: ";
+$test17->test_jolo_api_bank_account_correctness_check();
 
 
 
