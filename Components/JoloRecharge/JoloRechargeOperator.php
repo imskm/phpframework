12b4936e3192f27181674dab12520162472f3a7f
Shek Muktar <im.skm07@gmail.com>
2019-04-10 19:09:32 +0530

    Added Circle codes property

--- Components/JoloRecharge/JoloRechargeOperator.php
+++ Components/JoloRecharge/JoloRechargeOperator.php
@@ -54,9 +54,74 @@ class JoloRechargeOperator
 		 */
 	];
 
+	private static $operators_number = [
+		/* ----------------------------------------------------------
+		 * Prepaid mobile recharge operators
+		 * ----------------------------------------------------------
+		 */
+		'AT'	=>	28,
+		'BS'	=>	3,
+		'BSS'	=>	3,
+		'IDX'	=>	8,
+		'JO'	=>	29,
+		'VF'	=>	22,
+		'TD'	=>	17,
+		'TDS'	=>	17,
+		'MTD'	=>	20,
+		'MTDS'	=>	20,
+		'MTM'	=>	6,
+		'MTMS'	=>	6,
+
+		/* ----------------------------------------------------------
+		 * DTH recharge operators
+		 * ----------------------------------------------------------
+		 */
+		'VT'	=>	95,
+		'SD'	=>	98,
+		'BT'	=>	96,
+		'TS'	=>	94,
+		'DT'	=>	97,
+		'AD'	=>	93,
+	];
+
+	/**
+	 * @var array  of circle codes
+	 */
+	private static $operators_circles = [
+		 0 => 'None',
+		 1 => 'Delhi/NCR',
+		 2 => 'Mumbai',
+		 3 => 'Kolkata',
+		 4 => 'Maharashtra',
+		 5 => 'Andhra Pradesh',
+		 6 => 'Tamil Nadu',
+		 7 => 'Karnataka',
+		 8 => 'Gujarat',
+		 9 => 'Uttar Pradesh (E)',
+		10 => 'Madhya Pradesh',
+		11 => 'Uttar Pradesh (W)',
+		12 => 'West Bengal',
+		13 => 'Rajasthan',
+		14 => 'Kerala',
+		15 => 'Punjab',
+		16 => 'Haryana',
+		17 => 'Bihar & Jharkhand',
+		18 => 'Orissa',
+		19 => 'Assam',
+		20 => 'North East',
+		21 => 'Himachal Pradesh',
+		22 => 'Jammu & Kashmir',
+		23 => 'Chennai',
+	];
+
 	public static function all()
 	{
 		return self::$operators;
+	}
+
+	public static function allCircles()
+	{
+		return self::$operators_circles;
 	}
 
 	public static function getByCode($code)
@@ -68,9 +133,33 @@ class JoloRechargeOperator
 		return self::$operators[$code];
 	}
 
+	/**
+	 * Gets operator number code
+	 *
+	 * @param $code string
+	 * @return int  on success valid operator id, on failure 0
+	 */
+	public static function getOperatorId($code)
+	{
+		if (self::isExist($code)) {
+			return 0;	/* None */
+		}
+
+		return self::$operators_number[$code];
+	}
+
 	public static function isExist($code)
 	{
 		return array_key_exists($code, self::$operators);
+	}
+
+	public static function getCircle($code)
+	{
+		if (!array_key_exists($code, self::$operators_circles)) {
+			return "";
+		}
+
+		return self::$operators_circles[$code];
 	}
 
 	
